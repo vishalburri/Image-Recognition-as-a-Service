cc00d6b669aa2e6a2aa004720025dbb15b27fa9a
Vishal Reddy Burri (Student) <vburri@sundevils.asu.edu>
2023-02-01 15:09:39 -0700

    Updated

--- app-tier/image_classification.py
+++ app-tier/image_classification.py
@@ -12,14 +12,15 @@ import sys
 import time
 
 
-def get_classified_image(local_path):
-    """_summary_
-
+def get_classified_image(local_path: str) -> str:
+    """
+    get_classified_image classfies the given image in path using a pretrained DL model
+    and returns the classified result
     Args:
-        local_path (_type_): _description_
+        local_path (str): Local path in ec2 instance where image is stored
 
     Returns:
-        _type_: _description_
+        str: (image_key, classified_result)
     """
     img = Image.open(local_path)
     model = models.resnet18(pretrained=True)
@@ -32,7 +33,7 @@ def get_classified_image(local_path):
     with open('./imagenet-labels.json') as f:
         labels = json.load(f)
     result = labels[np.array(predicted)[0]]
-    img_name = url.split("/")[-1]
+    img_name = local_path.split("/")[-1]
     # save_name = f"({img_name}, {result})"
     save_name = f"{img_name},{result}"
     return (f"{save_name}")
