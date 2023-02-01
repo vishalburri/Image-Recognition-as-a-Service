772f702644605aeea520c01e9991bb03dab558de
Vishal Reddy Burri (Student) <vburri@sundevils.asu.edu>
2023-02-01 01:23:36 -0700

    Start Code

---
+++ app-tier/app.py
@@ -0,0 +1,80 @@
+import os
+import boto3
+from s3_url import S3Url
+from image_classification import classify
+import time
+from datetime import datetime, timezone
+import subprocess
+import requests
+import json
+# Define AWS Region
+aws_region = 'us-east-1'
+# SQS Queue URL
+queue_url = 'https://sqs.us-east-1.amazonaws.com/170322465562/queue'
+
+results_s3 = "cse546-project1-output"
+
+# Instantiate Clients
+sqs = boto3.client('sqs', region_name=aws_region)
+s3 = boto3.client('s3')
+ec2 = boto3.client('ec2', region_name=aws_region)
+dynamodb = boto3.resource('dynamodb', region_name=aws_region,
+                          endpoint_url='https://dynamodb.us-east-1.amazonaws.com')
+# Obtain Instance ID of instance
+r = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+instance_id = r.text
+
+
+def get_latest_message():
+    """ Gets the first available message in queue """
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=['All'],
+        VisibilityTimeout=10,
+        WaitTimeSeconds=0
+    )
+    receipt_handle = response['Messages'][0]['ReceiptHandle']
+    s3_object_path = json.loads(response['Messages'][0]['Body'])
+    bucket = s3_object_path['Records'][0]['s3']['bucket']['name']
+    key = s3_object_path['Records'][0]['s3']['object']['key']
+    s3_object_path = "s3://{}/{}".format(bucket, key)
+    return s3_object_path, receipt_handle
+
+
+def delete_message(receipt_handle):
+    """ Deletes the SQS message that matches the specified handle """
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
+
+
+def process_image(s3_object_path):
+    s = S3Url(s3_object_path)
+    s3.download_file(s.bucket, s.key, f'/tmp/{s.key}')
+
+    result = classify(f'/tmp/{s.key}')
+
+    os.remove(f'/tmp/{s.key}')
+
+    print(result)
+
+
+def run_job():
+    while get_num_messages_available() > 0:
+        try:
+            print("Retrieving Image Link from SQS")
+            s3_object_path, receipt_handle = get_latest_message()
+            process_image(s3_object_path)
+            delete_message(receipt_handle)
+            print("Successfully Processed {}".format(s3_object_path))
+            # time.sleep(1)
+        except:
+            print("No more messages available")
+            time.sleep(2)
+
+
+while True:
+    run_job()
+    time.sleep(5)  # poll every 5 seconds
