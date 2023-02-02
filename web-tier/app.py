407afc31104f68c200c2ff093ef3122b986e0d36
Vishal Reddy Burri (Student) <vburri@sundevils.asu.edu>
2023-02-02 15:32:38 -0700

    Updated

--- web-tier/app.py
+++ web-tier/app.py
@@ -1,27 +1,13 @@
 from fastapi import FastAPI, UploadFile, HTTPException, Request
-import boto3
 from fastapi.templating import Jinja2Templates
+from s3_client import S3Client
 
 app = FastAPI()
 
 # Load the Jinja2 template
 templates = Jinja2Templates(directory="templates")
-
-
-def upload_file_to_s3(file: UploadFile, bucket_name):
-    s3 = boto3.client("s3")
-    try:
-        s3.upload_fileobj(
-            file.file,
-            bucket_name,
-            file.filename,
-            ExtraArgs={
-                "ContentType": file.content_type
-            }
-        )
-    except Exception as e:
-        print("Something Happened: ", e)
-        return e
+INPUT_S3_BUCKET = 'cse546-project1-input-s3'
+s3_client = S3Client()
 
 
 @app.get("/")
@@ -31,8 +17,7 @@ def index(request: Request):
 
 @app.post("/")
 def upload_files(files: list[UploadFile] | None):
-    for uploaded_file in files:
-        print(uploaded_file.filename)
-        if uploaded_file.filename != '':
-            upload_file_to_s3(uploaded_file, 'cse546-project1-input-s3')
+    for file in files:
+        if file.filename != '':
+            s3_client.upload_file_to_s3(file, INPUT_S3_BUCKET)
     raise HTTPException(status_code=302, headers={"Location": "/"})
---
+++ web-tier/s3_client.py
@@ -0,0 +1,26 @@
+import boto3
+from fastapi import UploadFile
+
+
+class S3Client:
+    def __init__(self):
+        """_summary_
+        """
+        self.client = boto3.client('s3')
+
+    def upload_to_s3(self, file: UploadFile, bucket_name: str):
+        """
+        Uploads the given input file to s3 bucket
+
+        Args:
+            file (UploadFile): _description_
+            bucket_name (str): _description_
+        """
+        self.client.s3.upload_fileobj(
+            file.file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ContentType": file.content_type
+            }
+        )
