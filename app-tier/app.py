db0da32120906602abab2183af73b15c410fdb9f
Vishal Reddy Burri (Student) <vburri@sundevils.asu.edu>
2023-02-02 12:04:12 -0700

    Updated

--- app-tier/app.py
+++ app-tier/app.py
@@ -1,29 +1,38 @@
 import time
 from sqs_client import SqsClient
+from s3_client import S3Client
 from image_processor import ImageProcessor
+import logging
 
 
 def process_image(sqs: SqsClient, image_processor: ImageProcessor) -> None:
     while sqs.get_approximate_messages_from_queue() > 0:
         try:
-            print("Retrieving Image Link from SQS")
+            logging.info("Received Image link from SQS")
+            start_time = time.time()
             s3_object_path, receipt_handle = sqs.get_message_from_queue()
-            image_processor.process_image(s3_object_path)
+            image_processor.process(s3_object_path)
             sqs.delete_message_from_queue(receipt_handle)
-            print("Successfully Processed {}".format(s3_object_path))
-        except:
-            print("No more messages available")
+            total_time = time.time() - start_time
+            logging.info(
+                f"Successfully Processed {s3_object_path} in {total_time} seconds")
+        except Exception as e:
+            logging.info("Messages not available", e)
             time.sleep(2)
 
 
 def run_polling_job() -> None:
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+    logging.info("Running Polling Job")
     sqs = SqsClient()
-    image_processor = ImageProcessor(sqs)
+    s3_client = S3Client()
+    image_processor = ImageProcessor(s3_client, sqs)
 
     while True:
-        print("Running Job")
         process_image(sqs, image_processor)
-        time.sleep(5)  # poll every 5 seconds
+        # poll every 5 seconds
+        time.sleep(5)
 
 
 run_polling_job()
