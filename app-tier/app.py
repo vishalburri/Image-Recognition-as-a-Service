f394845cd30b11b59394086f7cb3b79c17103fc8
Vishal Reddy Burri (Student) <vburri@sundevils.asu.edu>
2023-02-15 13:20:46 -0700

    Updated

--- app-tier/app.py
+++ app-tier/app.py
@@ -2,12 +2,16 @@ import time
 from sqs_client import SqsClient
 from s3_client import S3Client
 from image_processor import ImageProcessor
+import constants
+import requests
+import boto3
 # import logging
+
+ec2_client = boto3.client('ec2', region_name=constants.AWS_REGION)
 
 
 def process_image(sqs: SqsClient, image_processor: ImageProcessor) -> None:
     try:
-        # logging.info("Received Image link from SQS")
         start_time = time.time()
         s3_object_path, receipt_handle = sqs.get_message_from_queue()
         print(f"s3 path : {s3_object_path}")
@@ -15,9 +19,16 @@ def process_image(sqs: SqsClient, image_processor: ImageProcessor) -> None:
             image_processor.process(s3_object_path)
             # time.sleep(0.5)
             sqs.delete_message_from_queue(receipt_handle)
-        total_time = time.time() - start_time
-        # logging.info(
-        # f"Successfully Processed {s3_object_path} in {total_time} seconds")
+        else:
+            try:
+                # terminate instance if no message is found
+                ec2_client.terminate_instances(InstanceIds=[r.text])
+                time.sleep(2)
+            except Exception as err:
+                print(err)
+            # total_time = time.time() - start_time
+            # logging.info(
+            # f"Successfully Processed {s3_object_path} in {total_time} seconds")
     except Exception as e:
         print(e)
         # logging.info("Messages not available", e)
@@ -37,4 +48,5 @@ def run_polling_job() -> None:
         time.sleep(2)
 
 
+r = requests.get(constants.INSTANCE_META_DATA_URL)
 run_polling_job()
